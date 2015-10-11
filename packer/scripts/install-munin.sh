cf2a213a494bd61b481ccee363221845de6f30d5
Charles Hooper <chooper@plumata.com>
2015-10-11 09:29:26 -0700

    Install missing perl dependency (munin)

--- packer/scripts/install-munin.sh
+++ packer/scripts/install-munin.sh
@@ -2,6 +2,6 @@
 
 set -eux -o pipefail
 
-sudo apt-get -y install munin-node munin-plugins-core munin-plugins-extra libnet-cidr-perl
+sudo apt-get -y install munin-node munin-plugins-core munin-plugins-extra libnet-cidr-perl libwww-perl
 sudo bash -c 'echo "cidr_allow 162.243.134.207/32" >> /etc/munin/munin-node.conf'
 sudo service munin-node restart
