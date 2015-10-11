878c772519a687b0e1354b0e9660243e4dcf45ae
Charles Hooper <chooper@plumata.com>
2015-10-11 09:08:10 -0700

    Monitor nginx

--- packer/scripts/install-httpd.sh
+++ packer/scripts/install-httpd.sh
@@ -4,3 +4,23 @@ set -eux -o pipefail
 
 sudo apt-get -y install nginx
 sudo service nginx status
+
+cat > /tmp/nginx-status.conf <<EOF
+server {
+  listen 127.0.0.1;
+  server_name localhost;
+  location /nginx_status {
+    stub_status on;
+    access_log   off;
+    allow 127.0.0.1;
+    deny all;
+  }
+}
+EOF
+
+sudo mv /tmp/nginx-status.conf /etc/nginx/conf.d/
+sudo service nginx reload
+
+sudo bash -c 'ln -s /usr/share/munin/plugins/nginx_request /etc/munin/plugins/nginx_request'
+sudo bash -c 'ln -s /usr/share/munin/plugins/nginx_request /etc/munin/plugins/nginx_status'
+sudo service munin-node restart
