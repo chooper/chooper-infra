b4b2cdd4e97c5b3674d50d346699317c4e3357bf
Charles Hooper <chooper@plumata.com>
2016-09-17 08:42:59 -0700

    I think this was a fixed golang package?

--- packer/scripts/install-golang.sh
+++ packer/scripts/install-golang.sh
@@ -2,9 +2,7 @@
 
 set -eux -o pipefail
 
-sudo apt-add-repository -y ppa:evarlast/golang1.5
+sudo apt-add-repository -y ppa:ubuntu-lxc/lxd-stable
 sudo apt-get -y update
-# HACK(charles) Broken golang package
-sudo apt-get -y install golang-go || true
-sudo apt-get -y install golang-go
+sudo apt-get -y install golang
 go version
