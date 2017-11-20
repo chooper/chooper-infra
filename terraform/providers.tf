47cef210ef9f84336b5ac6b05693ccb19dd1ad0a
Charles Hooper <chooper@plumata.com>
2017-11-20 13:51:55 -0800

    Add a kubernetes provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,5 +1,18 @@
 provider "google" {
-  project     = "key-chalice-143715"
-  region      = "us-west1"
-  version     = "~> 1.2"
+  version = "~> 1.2"
+
+  project = "key-chalice-143715"
+  region  = "us-west1"
+}
+
+provider "kubernetes" {
+  version = "~> 1.0"
+
+  username = "${var.cluster_master_username}"
+  password = "${var.cluster_master_password}"
+  host     = "http://${google_container_cluster.primary.endpoint}"
+
+  client_certificate     = "${google_container_cluster.primary.master_auth.0.client_certificat}"
+  client_key             = "${google_container_cluster.primary.master_auth.0.client_key}"
+  cluster_ca_certificate = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
 }
